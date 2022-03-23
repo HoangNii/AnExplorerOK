14d3b5bfa96b81a5a68f1434e18cce8668ffdf5d
Duchoangitt <Duchoangitt@gmail.com>
2022-03-23 09:18:53 +0700

    Initial commit

---
+++ app/src/free/java/dev/dworks/apps/anexplorer/misc/CrashReportingManager.java
@@ -0,0 +1,30 @@
+package dev.dworks.apps.anexplorer.misc;
+
+import android.content.Context;
+
+/**
+ * Created by HaKr on 23/05/16.
+ */
+
+public class CrashReportingManager {
+
+    public static void enable(Context context, boolean enable){
+
+    }
+
+    public static void logException(Exception e) {
+        logException(e, false);
+    }
+
+    public static void logException(Exception e, boolean log) {
+
+    }
+
+    public static void log(String s) {
+
+    }
+
+    public static void log(String tag, String s) {
+
+    }
+}
